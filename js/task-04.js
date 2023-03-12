c04d01ae183616a1ca9c62d1a9703fc6b184040a
Микола Іванишин <43409421+Mikaeri@users.noreply.github.com>
2023-03-12 23:38:53 +0000

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,15 @@
+let counterValue = 0;
+
+const decrement = document.querySelector('[data-action="decrement"]');
+const increment = document.querySelector('[data-action="increment"]');
+const initialValue = document.querySelector("#value");
+
+decrement.addEventListener("click", () => {
+  counterValue -= 1;
+  initialValue.textContent = counterValue;
+});
+
+increment.addEventListener("click", () => {
+  counterValue += 1;
+  initialValue.textContent = counterValue;
+});
