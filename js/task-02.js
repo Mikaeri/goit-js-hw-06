ec3caf4ee1c25449572c7b4e9dddf73fb9c624bd
Микола Іванишин <43409421+Mikaeri@users.noreply.github.com>
2023-03-12 22:16:01 +0000

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const list = document.querySelector('#ingredients');
+
+const elements = ingredients.map(element =>{
+const newElement = document.createElement("li");
+newElement.textContent = element;
+newElement.className = 'item';
+console.log(newElement);
+return newElement;
+});
+
+list.append(...elements);
+
+
+
