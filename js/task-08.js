098ed208d9706bfddc15b819f3835a80bb391989
Микола Іванишин <43409421+Mikaeri@users.noreply.github.com>
2023-03-13 23:45:22 +0000

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const loginForm = document.querySelector('.login-form');
+
+loginForm.addEventListener('submit', (event) => {
+event.preventDefault();
+const elements = event.target.elements;
+const formData = {};
+
+for (const element of elements){
+    if (element.nodeName == 'INPUT'){
+        if (!element.value){
+            alert("Всі поля мають бути заповнені");
+            return;
+        }
+    formData[element.name] = element.value;
+    }}
+    console.log(formData);
+    loginForm.reset();
+})
+
+
+
+
 
