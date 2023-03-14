3282fc5b96c7d0483ab1e0c8ec00a310829964d1
Микола Іванишин <43409421+Mikaeri@users.noreply.github.com>
2023-03-14 00:32:00 +0000

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,31 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const createBoxes = () => {
+  const amount = input.value;
+  let divSize = 30;
+
+  for (let i = 0; i<amount; i+=1){
+  const boxDiv = document.createElement('div');
+  boxDiv.style.width = `${divSize}px`;
+  boxDiv.style.height =`${divSize}px`;
+  boxDiv.style.backgroundColor = getRandomHexColor();
+
+  divSize+=10;
+}}
+
+const destroyBoxes = () =>{
+  boxes.innerHTML = '';
+}
+
+const input = document.querySelector('#controls input');
+const createEl = document.querySelector('[data-create]');
+const destroyEl = document.querySelector('[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+createEl.addEventListener('click', createBoxes);
+destroyEl.addEventListener('click', destroyBoxes);
+
+
+
