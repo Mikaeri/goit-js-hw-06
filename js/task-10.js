52725ee1d2d3fa4cd1c568f40beb598bdd52ec8b
Микола Іванишин <43409421+Mikaeri@users.noreply.github.com>
2023-03-21 21:16:10 +0000

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,33 +1,38 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
-
-const createBoxes = () => {
-  const amount = input.value;
-  let divSize = 30;
-
-  for (let i = 0; i<amount; i+=1){
-  const boxDiv = document.createElement('div');
-  boxDiv.style.width = `${divSize}px`;
-  boxDiv.style.height =`${divSize}px`;
-  boxDiv.style.backgroundColor = getRandomHexColor();
-
-  divSize+=10;
-}}
-
-const destroyBoxes = () =>{
-  boxes.innerHTML = '';
-}
-
-const input = document.querySelector('#controls input');
-const createEl = document.querySelector('[data-create]');
-const destroyEl = document.querySelector('[data-destroy]');
-const boxes = document.querySelector('#boxes');
-
-createEl.addEventListener('click', createBoxes);
-destroyEl.addEventListener('click', destroyBoxes);
-
-
-
+const refs = {
+  input: document.querySelector('#controls input'),
+  createBtn: document.querySelector('[data-create]'),
+  destroyBtn: document.querySelector('[data-destroy]'),
+  boxesDiv: document.querySelector('#boxes'),
+  };
+  
+  refs.createBtn.addEventListener('click', createBoxes);
+  refs.destroyBtn.addEventListener('click', destroyBoxes);
+  
+  function createBoxes() {
+  const boxSize = 30;
+  const amount = refs.input.value;
+  
+  const boxes = Array.from({ length: amount }, (_, index) => {
+  const box = document.createElement('div');
+  box.style.width = `${boxSize + index * 10}px`;
+  box.style.height = `${boxSize + index * 10}px`;
+  box.style.backgroundColor = getRandomHexColor();
+  return box;
+  });
+  
+  refs.boxesDiv.append(...boxes);
+  }
+  
+  function destroyBoxes() {
+  refs.boxesDiv.innerHTML = '';
+  }
+  
+  function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+  }
+  
+  
+  
+  
+  
+  
