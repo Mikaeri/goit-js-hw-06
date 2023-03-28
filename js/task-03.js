0f656a9c5a3f8ca671a6aeb4b72027b02be84da5
Микола Іванишин <43409421+Mikaeri@users.noreply.github.com>
2023-03-28 21:38:18 +0100

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -1,14 +1,14 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    src: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'White and Black Long Fur Cat',
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    src: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    src: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
   },
 ];
@@ -17,7 +17,7 @@ const ul = document.querySelector('.gallery');
 
 const listElements = images.map(element =>{
   const newElement = document.createElement("li");
-  return `<li class ="task-3-item"><img class="task-3-img" url="${element.url}" alt = "${element.alt}"></img></li>`
+  return `<li class ="task-3-item"><img class="task-3-img" src="${element.src}" alt = "${element.alt}"></img></li>`
 });
 
 ul.insertAdjacentHTML('afterbegin', listElements);
